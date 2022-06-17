17bc385ab6ee0f4fc12e33a5d80abc2bb7e85fe2
User <ostashkovladislav@yandex.ru>
2022-06-17 19:33:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pr14ostashkopr_20101/MainActivity6.kt
@@ -0,0 +1,11 @@
+package com.example.pr14ostashkopr_20101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main6)
+    }
+}
